a160cb731702a01980afe6e99685abbd7ad7061f
w3ttheonlyone <fabian.meyer1337@web.de>
2014-11-19 23:17:04 +0100

    testing

--- shader/computeShader.glsl
+++ shader/computeShader.glsl
@@ -21,9 +21,10 @@ layout(std140, binding = 3) buffer gVel {
 };
 
 
-layout(local_size_x= 1024, local_size_y =1, local_size_z = 1)in;
+layout(local_size_x= 64, local_size_y =1, local_size_z = 1)in;
 
 int width = 4;
+ivec3 windowOffset=ivec3(-2,-2,-2);
 uint threadNum = 64;
 int n= 0;
 void getIndex(inout ivec3 ijk,inout int index){
@@ -31,7 +32,7 @@ void getIndex(inout ivec3 ijk,inout int index){
 }
 void getIJK(inout ivec3 ijk, inout int index){
     int temp = index%(width*width);
-    ijk= ivec3(temp%width,temp/width,index/(width*width));
+    ijk= ivec3(temp%width,temp/width,index/(width*width))+windowOffset;
 }
 
 void main(void){
@@ -42,15 +43,20 @@ void main(void){
     int gridOffsetOfParticle = int(globalInvoc%threadNum); //  597%64=21
     ivec3 gridOffset;
 
-    getIJK(gridOffset,gridOffsetOfParticle ); // temp = 21%
+    getIJK(gridOffset,gridOffsetOfParticle ); // temp = 21%16 = 5, ijk=(5%4, 5/4, 21/16) = (1,1,1)+(-2,-2,-2) = (-1,-1,-1)
 
     ivec3 gridIndex = ivec3((pPositionsMass[pIndex].xyz- gGridPos)/gridSpacing) + gridOffset;
+    // gridIndex= ivec3(((pPositionsMass[9].xyz - (-1.05,0.05,-1.05))/0.1) + (-1,-1,-1)
+    //          = ivec3(((0.4,0.5,-0.5)-(-1.05,0.05,-1.05))/0.1))+(-1,-1,-1)
+    //          = (1.45,0.55,0.55)/0.1 +(-1,-1,-1)
+    //          = (14.5,5.5,5.5) +(-1,-1,-1)
+    //          = (13.5,4.5,4.5)
     if(gridIndex.x>= n && gridIndex.y>=n && gridIndex.z>=n && gridIndex.x< gGridDim[0].x && gridIndex.y <gGridDim[1].x &&gridIndex.z< gGridDim[2].x ){
 
 
         int gPositionsMassIndex = 0;
         getIndex(gridIndex,gPositionsMassIndex);
-
+    // gPositionsMassIndex = 13+ 4*100+4*100*100 = 40413
         gPositionsMass[gPositionsMassIndex].w+=pPositionsMass[pIndex].w; // 87 Fps
         barrier();
         //gVelocities[gPositionsMassIndex].xyz+=pVelocities[pIndex].xyz; //27 Fps
--- src/simulation/timeUpdate/explicittimeupdate.cpp
+++ src/simulation/timeUpdate/explicittimeupdate.cpp
@@ -23,7 +23,7 @@ void ExplicitTimeUpdate::update(double dt){
     pc.setGridDim(grid->dimx, grid->dimy, grid->dimz);
     pc.setdt(dt);
     pc.setGridSpacing(grid->h);
-    glDispatchCompute(2048,1,1);
-    //glMemoryBarrier ( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+    glDispatchCompute(32*32*32,1,1);
+    glMemoryBarrier ( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
 
 }
