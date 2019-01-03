8028483a996e87ae63dd9b08a81a86616efcff69
MeyerFabian <fabian.meyer1337@web.de>
2019-01-03 13:48:22 +0100

    loading looks easier now

--- shader/test/shared/p2g_atomic.glsl
+++ shader/test/shared/p2g_atomic.glsl
@@ -9,7 +9,6 @@ layout(local_size_x =X, local_size_y =Y,local_size_z =Z)in;
 #define HALO_X (VOXEL_DIM_X+LEFT_SUPPORT+RIGHT_SUPPORT)
 #define HALO_Y (VOXEL_DIM_Y+LEFT_SUPPORT+RIGHT_SUPPORT)
 #define HALO_Z (VOXEL_DIM_Z+LEFT_SUPPORT+RIGHT_SUPPORT)
-#define THREAD_RANGE int((HALO_X*HALO_Y*HALO_Z)/(X*Y*Z))
 shared PREC_VEC_TYPE temp [HALO_X*HALO_Y*HALO_Z];
 
 #define blockSize uvec3(VOXEL_DIM_X,VOXEL_DIM_Y,VOXEL_DIM_Z)
@@ -29,15 +28,10 @@ void main(void){
 
 	int local_i = int(gl_LocalInvocationID.x);
 
-	for(int frac = 0; frac < THREAD_RANGE;frac++){
-		temp[local_i] = PREC_VEC_TYPE(0.0);
-		local_i += X*Y*Z;
-	}
-
-
-	if(local_i < (HALO_X*HALO_Y*HALO_Z)) {
+	for(int local_i = int(gl_LocalInvocationID.x); local_i < (HALO_X*HALO_Y*HALO_Z);local_i += X*Y*Z) {
 		temp[local_i] = PREC_VEC_TYPE(0.0);
 	}
+
 	memoryBarrierShared();
 	barrier();
 	for(int particle_i = 0; particle_i < count; particle_i++){
@@ -74,10 +68,10 @@ void main(void){
 	memoryBarrierShared();
 	barrier();
 
-	local_i = int(gl_LocalInvocationID.x);
 	ivec3 grid_start_node = ivec3(blockID * blockSize) - LEFT_SUPPORT;
 
-	for(int frac = 0; frac < THREAD_RANGE;frac++){
+
+	for(int local_i = int(gl_LocalInvocationID.x); local_i < (HALO_X*HALO_Y*HALO_Z);local_i += X*Y*Z) {
 		uvec3 halo_ijk = uvec3(getIJK(local_i,ivec3(HALO_X,HALO_Y,HALO_Z)));
 		ivec3 to_process = grid_start_node + ivec3(halo_ijk);
 		if(inBounds(to_process,grid_def.gGridDim)){
@@ -96,29 +90,5 @@ void main(void){
 			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).w,
 					to_write.w);
 		}
-		local_i += X*Y*Z;
 	}
-
-	if(local_i < (HALO_X*HALO_Y*HALO_Z)) {
-		uvec3 halo_ijk = uvec3(getIJK(local_i,ivec3(HALO_X,HALO_Y,HALO_Z)));
-		ivec3 to_process = grid_start_node + ivec3(halo_ijk);
-		if(inBounds(to_process,grid_def.gGridDim)){
-			uint write_key = SORTING_KEY(to_process,grid_def.gGridDim);
-			PREC_VEC_TYPE to_write = temp[local_i];
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).x,
-					to_write.x);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).y,
-					to_write.y);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).z,
-					to_write.z);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).w,
-					to_write.w);
-		}
-
-	}
-
 }
--- shader/test/shared/p2g_atomic_batching.glsl
+++ shader/test/shared/p2g_atomic_batching.glsl
@@ -9,7 +9,6 @@ layout(local_size_x =X, local_size_y =Y,local_size_z =Z)in;
 #define HALO_X (VOXEL_DIM_X+LEFT_SUPPORT+RIGHT_SUPPORT)
 #define HALO_Y (VOXEL_DIM_Y+LEFT_SUPPORT+RIGHT_SUPPORT)
 #define HALO_Z (VOXEL_DIM_Z+LEFT_SUPPORT+RIGHT_SUPPORT)
-#define THREAD_RANGE int((HALO_X*HALO_Y*HALO_Z)/(X*Y*Z))
 shared PREC_VEC_TYPE temp [HALO_X*HALO_Y*HALO_Z];
 
 #define blockSize uvec3(VOXEL_DIM_X,VOXEL_DIM_Y,VOXEL_DIM_Z)
@@ -27,17 +26,11 @@ void main(void){
 
 	uint scan = INPUT_SCAN_AT(INPUT_SCAN,INPUT_SCAN_VAR,INPUT_SCAN_SIZE,grid_key,INPUT_SCAN_NUM_BUFFER,INPUT_SCAN_INDEX_BUFFER);
 
-	int local_i = int(gl_LocalInvocationID.x);
 
-	for(int frac = 0; frac < THREAD_RANGE;frac++){
-		temp[local_i] = PREC_VEC_TYPE(0.0);
-		local_i += X*Y*Z;
-	}
-
-
-	if(local_i < (HALO_X*HALO_Y*HALO_Z)) {
+	for(int local_i = int(gl_LocalInvocationID.x); local_i < (HALO_X*HALO_Y*HALO_Z);local_i += X*Y*Z) {
 		temp[local_i] = PREC_VEC_TYPE(0.0);
 	}
+
 	memoryBarrierShared();
 	barrier();
 
@@ -144,10 +137,9 @@ void main(void){
 	memoryBarrierShared();
 	barrier();
 
-	local_i = int(gl_LocalInvocationID.x);
 	ivec3 grid_start_node = ivec3(blockID * blockSize) - LEFT_SUPPORT;
 
-	for(int frac = 0; frac < THREAD_RANGE;frac++){
+	for(int local_i = int(gl_LocalInvocationID.x); local_i < (HALO_X*HALO_Y*HALO_Z);local_i += X*Y*Z) {
 		uvec3 halo_ijk = uvec3(getIJK(local_i,ivec3(HALO_X,HALO_Y,HALO_Z)));
 		ivec3 to_process = grid_start_node + ivec3(halo_ijk);
 		if(inBounds(to_process,grid_def.gGridDim)){
@@ -166,29 +158,5 @@ void main(void){
 			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).w,
 					to_write.w);
 		}
-		local_i += X*Y*Z;
 	}
-
-	if(local_i < (HALO_X*HALO_Y*HALO_Z)) {
-		uvec3 halo_ijk = uvec3(getIJK(local_i,ivec3(HALO_X,HALO_Y,HALO_Z)));
-		ivec3 to_process = grid_start_node + ivec3(halo_ijk);
-		if(inBounds(to_process,grid_def.gGridDim)){
-			uint write_key = SORTING_KEY(to_process,grid_def.gGridDim);
-			PREC_VEC_TYPE to_write = temp[local_i];
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).x,
-					to_write.x);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).y,
-					to_write.y);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).z,
-					to_write.z);
-
-			atomicAdd(OUTPUT_AT(OUTPUT,Gridpoint_vel_mass,OUTPUT_SIZE,write_key,OUTPUT_NUM_BUFFER,OUTPUT_INDEX_BUFFER).w,
-					to_write.w);
-		}
-
-	}
-
 }
